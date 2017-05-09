20e67482c04b261acc5b9b87a7bc920d3ef28e2e
David Jankoski <david.jankoski@hellotrip.nl>
2017-05-10 00:03:47 +0200

    better filepath construction and R_HOME calling

--- scheduled_task/schedule_scraper.R
+++ scheduled_task/schedule_scraper.R
@@ -6,15 +6,14 @@
 
 library("taskscheduleR")
 
-# path to where this project sits
-proj_path <- "C:/Users/DavidJankoski/Desktop/r_files/packt_book_of_day/" 
-
+# path to Rscript.exe
+rscript_path <- file.path( R.home(), "bin", "Rscript.exe" )
 # path to the check-and-run script
-script_path <- paste0(proj_path, "scheduled_task/check_and_run_scraper.R")
-  
+script_path <- file.path( getwd(), "scheduled_task", "check_and_run_scraper.R" )
+
 # schedule script to run 
 taskscheduler_create(taskname = "packt_book_scraper", 
                      rscript = script_path, 
                      schedule = "ONLOGON", 
-                     Rexe = "C:/Program Files/R/R-3.3.0/bin/Rscript.exe", 
+                     Rexe = rscript_path, 
                      starttime = Sys.Date() + 1)
