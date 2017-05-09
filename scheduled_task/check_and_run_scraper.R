06639ec69b018cb944182cedc67e73aa51807bdf
David Jankoski <david.jankoski@hellotrip.nl>
2017-05-10 00:03:22 +0200

    more general way of calling R_HOME added

--- scheduled_task/check_and_run_scraper.R
+++ scheduled_task/check_and_run_scraper.R
@@ -2,17 +2,15 @@
 # already exists in the img/ dir - and runs if no such file name 
 # was found.
 
-# path to where this project sits
-proj_path <- "C:/Users/DavidJankoski/Desktop/r_files/packt_book_of_day/" 
-
 # path to todays img
-todays_img <- paste0(proj_path, "img/image_" , format(Sys.Date(), "%d%m%g"), ".png")
+today_img <- paste0( "image_" , format( Sys.Date(), "%d%m%g" ), ".png" )
+todays_img <- file.path( getwd(), "img", today_img )
 
-if (!file.exists(todays_img)) {
+if ( !file.exists(todays_img) ) {
   # path to rscript.exe
-  r_path <- "C:/Program Files/R/R-3.3.0/bin/Rscript.exe"
+  rscript_path <- file.path( R.home(), "bin", "Rscript.exe" )
   # path to script
-  script_path <- paste0(proj_path, "packt_book_deal_scraper.R")
+  script_path <- file.path( getwd(), "packt_book_deal_scraper.R" )
   # run
-  system2(r_path, script_path)
+  system2(rscript_path, script_path)
 }
